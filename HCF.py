38ca5d7ed38de95ea1060a21cb0deb24315420f6
Lakshmi Vara Prasad Kuna <43074474+klvp@users.noreply.github.com>
2026-06-20 11:29:38 +0530

    Fix indentation in compute_hcf function

--- HCF.py
+++ HCF.py
@@ -4,7 +4,8 @@
 def compute_hcf(x, y):
 
 # choose the smaller number
-    if x > y:smaller = y
+    if x > y:
+        smaller = y
     else:
         smaller = x
     for i in range(1, smaller+1):
